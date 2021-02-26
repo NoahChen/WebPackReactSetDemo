8f8f3ef8cdb685e6fcdba8f530806fa05de7dff8
NoahChen <noachen666@163.com>
2021-02-26 11:13:25 +0800

    no message

--- noah-web-app/src/App.js
+++ noah-web-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class App extends React.Component {
     render() {
         return (
-            <div>webpack react 配置demo</div>
+            <div>webpack在react上的简单配置demo</div>
         )
     }
 }
